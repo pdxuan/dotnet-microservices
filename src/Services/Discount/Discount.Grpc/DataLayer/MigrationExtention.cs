811c347cd975e06b47df651991acd2961efbd512
Xuan-Spider <dinhxuan9807@gmail.com>
2024-09-17 16:57:13 +0700

    feat: add EFCore for the Discount service

---
+++ src/Services/Discount/Discount.Grpc/DataLayer/MigrationExtention.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.DataLayer
+{
+    public static class MigrationExtention
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace Discount.Grpc.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public required string ProductName { get; set; }
+
+        public string? Description { get; set; } = default;
+
+        public int Amount { get; set; }
+    }
+}
