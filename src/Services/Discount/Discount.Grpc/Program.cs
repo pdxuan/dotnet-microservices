0db06fca0e1874ade4760543dd6d04f27d25d040
Xuan-Spider <dinhxuan9807@gmail.com>
2024-09-17 21:45:20 +0700

    feat: add CRUD cupon operation

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddDbContext<DiscountContext>(opts =>
 var app = builder.Build();
 
 
-app.UseMigration();
 // Configure the HTTP request pipeline.
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
