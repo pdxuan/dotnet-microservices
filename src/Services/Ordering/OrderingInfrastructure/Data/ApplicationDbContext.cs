2bde784911bdbbfc4bbf6b0ccefb9286fb593720
Xuan-Spider <dinhxuan9807@gmail.com>
2024-09-23 23:39:02 +0700

    feat: add Entity framwork core for the Ordering service

---
+++ src/Services/Ordering/OrderingInfrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderingDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingInfrastructure.Data
+{
+    public class ApplicationDbContext : DbContext   
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+        
+        }
+
+
+        public DbSet<Customer> Customers  => Set<Customer>();
+
+        public DbSet<Product> Products => Set<Product>();
+
+        public DbSet<Order> Orders => Set<Order>();
+
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
